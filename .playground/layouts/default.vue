f906838823f880e2161bd3388a4925b9fd8d2a78
Madao <gn00678465@gmail.com>
2025-10-09 14:27:43 +0800

    feat(playground): 新增基本頁面結構與 HelloWorld 組件

---
+++ .playground/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <base-layout>
+    <template #header>
+      <div>header</div>
+    </template>
+    <template #tab>
+      <div>tab</div>
+    </template>
+    <template #sider>
+      <div>sider</div>
+    </template>
+    <slot></slot>
+    <template #footer>
+      <div>footer</div>
+    </template>
+  </base-layout>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
